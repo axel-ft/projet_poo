e4ca682c737b79f806c4d82f13eaab30c85cbd54
matthieu <matthieu@localhost.localdomain>
2017-01-31 14:23:39 +0100

    add main.cpp, missing variable

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namesapce std;
+
+int main()
+{
+	cout << "Société : " << //variable nom société << "\n" << endl;
+	cout << "Vendeurs : " << //variable noms vendeurs << "\n" << endl;
+	cout << "Représentants : " << //variable noms représentants << "\n" << endl;
+	cout << "Techniciens : " << //variables noms techniciens << "\n" << endl;
+	cout << "Intérimaires : " << //variables noms intérimaires << "\n" << endl;
+	cout << "Les salaires des employés ce mois ci \n" << endl;
+	cout << "Salaire de Paul : " << //variable salaire Paul << "\n" << endl;
+	cout << "Salaire de Pierre : " << //variable salaire de Pierre << "\n" << endl;
+	cout << "Salaire de Jacques : " << //variable salaire de Jacques << "\n" << endl;
+	cout << "Salaire de Rachel : " << //variable salaire de Rachel << "\n" << endl;
+	cout << "Salaire de Rémi : " << //variable salaire de Rémi << "\n" << endl;
+	cout << "Salaire de Idris : " << //variable salaire de Idris << "\n" << endl;
+	cout << "Salaire de Issac : " << //variable salaire de Isaac << "\n" << endl;
+	cout << "Salaire de Irène : " << //variable salaire de Irène << "\n" << endl;
+	cout << "Salaire de Igor : " << //variable salaire de Igor << "\n" << endl;
+}
+
+
+
+
+
+
+
+
+
