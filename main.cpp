c75a75630226122e83bead6ad6c2e83c885bc6e0
matthieu <matthieu@localhost.localdomain>
2017-01-31 16:06:23 +0100

    add main

--- main.cpp
+++ main.cpp
@@ -1,26 +1,31 @@
+#include <stdio.h>
 #include <iostream>
+#include <string>
+#include "entreprise.h"
+#include "employe.h"
+#include "commercial.h"
+#include "technicien.h"
+
 using namesapce std;
 
 int main()
 {
-	cout << "Société : " << //variable nom société << "\n" << endl;
-	cout << "Vendeurs : " << //variable noms vendeurs << "\n" << endl;
-	cout << "Représentants : " << //variable noms représentants << "\n" << endl;
-	cout << "Techniciens : " << //variables noms techniciens << "\n" << endl;
-	cout << "Intérimaires : " << //variables noms intérimaires << "\n" << endl;
-	cout << "Les salaires des employés ce mois ci \n" << endl;
-	cout << "Salaire de Paul : " << //variable salaire Paul << "\n" << endl;
-	cout << "Salaire de Pierre : " << //variable salaire de Pierre << "\n" << endl;
-	cout << "Salaire de Jacques : " << //variable salaire de Jacques << "\n" << endl;
-	cout << "Salaire de Rachel : " << //variable salaire de Rachel << "\n" << endl;
-	cout << "Salaire de Rémi : " << //variable salaire de Rémi << "\n" << endl;
-	cout << "Salaire de Idris : " << //variable salaire de Idris << "\n" << endl;
-	cout << "Salaire de Issac : " << //variable salaire de Isaac << "\n" << endl;
-	cout << "Salaire de Irène : " << //variable salaire de Irène << "\n" << endl;
-	cout << "Salaire de Igor : " << //variable salaire de Igor << "\n" << endl;
+	cout << "Société : " << Entreprise::getNom() << "\n" << endl;
+	cout << "Vendeurs : " << Entreprise::getVendeur() << "\n" << endl;
+	cout << "Représentants : " << Entreprise::getRepresentant() << "\n" << endl;
+	cout << "Techniciens : " << Entreprise::getTechnicien() << "\n" << endl;
+	cout << "Intérimaires : " << Entreprise::getInterimaire() << "\n" << endl;
+	cout << "Les salaires des employés ce mois ci sont :\n" << endl;
+	cout << "Salaire de Paul : " << Paul::calculSalaire() << "\n" << endl;
+	cout << "Salaire de Pierre : " << Pierre::calculSalaire() << "\n" << endl;
+	cout << "Salaire de Jacques : " << Jacques::calculSalaire() << "\n" << endl;
+	cout << "Salaire de Rachel : " << Rachel::calculSalaire() << "\n" << endl;
+	cout << "Salaire de Rémi : " << Remi::calculSalaire() << "\n" << endl;
+	cout << "Salaire de Idris : " << Idris::calculSalaire() << "\n" << endl;
+	cout << "Salaire de Issac : " << Isaac::calculSalaire() << "\n" << endl;
+	cout << "Salaire de Irène : " << Irene::calculSalaire() << "\n" << endl;
+	cout << "Salaire de Igor : " << Igor::calculSalaire() << "\n" << endl;
 }
-
-
 
 
 
